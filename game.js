621f5d8e7b22ac16d9eea8d23c7c4d2dbdd2a90f
Jibanjyot <jibanjyotikalita03@gmail.com>
2023-07-23 21:20:31 +0530

    completed

--- game.js
+++ game.js
@@ -1,39 +1,27 @@
-var ball_x, ball_y, ball_dx, ball_y;
-var paddle_y, paddle_y, paddle_width, paddle_height, paddle_dx;
-var brickRows = 4, brickColumns = 4, brick_width = 80, brick_height = 20, brick_padding = 15, brickOffsetLeft = 15, brickOffsetTop = 10;
-var score = 0;
+let ball_x, ball_y, ball_dx, ball_dy;
+let paddle_y, paddle_x, paddle_width, paddle_height, paddle_dx;
+const brickRows = 4, brickColumns = 4;
+const brick_width = 80, brick_height = 20, brick_padding = 15, brickOffsetLeft = 15, brickOffsetTop = 10;
+let score = 0;
+let bricks = [];
+let lost = false;
 
-var bricks = [];
-
-var lost = false;
-
-for (var c = 0; c < brickColumns; c++) {
-    bricks[c] = [];
-    for (var r = 0; r < brickRows; r++) {
-        bricks[c][r] = { x: 0, y: 0, hidden: 0 };
-    }
+function setup() {
+    createCanvas(400, 400);
+    initializeBricks();
+    resetGame();
 }
 
-function createBricks() {
-    for (var c = 0; c < brickColumns; c++) {
-        for (var r = 0; r < brickRows; r++) {
-            if (bricks[c][r].hidden === 0) {
-                const brick_x = c * (brick_width + brick_padding) + brickOffsetLeft;
-                const brick_y = r * (brick_height + brick_padding) + brickOffsetTop;
-                bricks[c][r].x = brick_x;
-                bricks[c][r].y = brick_y;
-                fill("black");
-                rect(bricks[c][r].x, bricks[c][r].y, brick_width, brick_height);
-            }
+function initializeBricks() {
+    for (let c = 0; c < brickColumns; c++) {
+        bricks[c] = [];
+        for (let r = 0; r < brickRows; r++) {
+            bricks[c][r] = { x: 0, y: 0, hidden: false };
         }
     }
 }
 
-
-
-function setup() {
-    createCanvas(400, 400);
-
+function resetGame() {
     ball_x = width / 2;
     ball_y = height / 2;
     ball_radius = 10;
@@ -45,73 +33,31 @@ function setup() {
     paddle_y = height - 30;
     paddle_x = width / 2 - paddle_width / 2
     paddle_dx = 4;
+
+    score = 0;
+    lost = false;
+    initializeBricks();
 }
 
-
-
-function horizontalHit() {
-    if(ball_x+ball_radius>= width  || ball_x <= ball_radius){
-        return true;
-    }
-    else{
-        return false;
-    }
-    
-}
-
-
-
-function gameOver() {
-    if(ball_y >= height - ball_radius / 2){
-        return true;
-    }
-    return false;
-}
-
-function paddleHit(){
-    if(ball_x>= paddle_x && ball_x<=paddle_x+paddle_width && ball_y>= paddle_y && ball_y<=paddle_y+paddle_width){
-        return true;
-    }
-    return false;
-}
-
-function verticalHit() {
-    if(ball_y < ball_radius / 2 || (ball_x <= paddle_x + paddle_width && ball_x >= paddle_x && ball_y >= paddle_y)){
-        return true;
-    }
-    return false;
-
-}
-
-
-
-function isBrickHit() {
-    for (var c = 0; c < brickColumns; c++) {
-        for (var r = 0; r < brickRows; r++) {
-            if (bricks[c][r].hidden === 1) {
-                continue;
-            }
-            if (bricks[c][r].y <= ball_y + ball_radius / 2 && bricks[c][r].y + brick_height >= ball_y + ball_radius / 2 && bricks[c][r].x <= ball_x + ball_radius / 2 && bricks[c][r].x + brick_width >= ball_x + ball_radius / 2) {
-
-                ball_dy = -ball_dy;
-                bricks[c][r].hidden = 1;
-
-                return true;
+function drawBricks() {
+    for (let c = 0; c < brickColumns; c++) {
+        for (let r = 0; r < brickRows; r++) {
+            if (!bricks[c][r].hidden) {
+                const brick_x = c * (brick_width + brick_padding) + brickOffsetLeft;
+                const brick_y = r * (brick_height + brick_padding) + brickOffsetTop;
+                bricks[c][r].x = brick_x;
+                bricks[c][r].y = brick_y;
+                fill("black");
+                rect(bricks[c][r].x, bricks[c][r].y, brick_width, brick_height);
             }
         }
     }
-    return false;
 }
 
-
-
 function draw() {
-    clear()
-    fill("black")
-
-    createBricks()
+    background(255);
+    drawBricks();
     circle(ball_x, ball_y, ball_radius);
-
     rect(paddle_x, paddle_y, paddle_width, paddle_height);
 
     if (keyIsDown(LEFT_ARROW)) {
@@ -126,33 +72,79 @@ function draw() {
         ball_dy = 0;
         ball_dx = 0;
         lost = true;
-    } 
-
-    if (isBrickHit()) {
-        score = score + 1;
     } else {
-        if (horizontalHit()) {
-            ball_dx = -ball_dx;
-        }
-        
-        if (verticalHit()) {
-            ball_dy = -ball_dy;
+        if (isBrickHit()) {
+            score++;
+        } else {
+            if (horizontalHit()) {
+                ball_dx = -ball_dx;
+            }
+            if (verticalHit()) {
+
+                ball_dy = -ball_dy;
+            }
         }
     }
-    text("Score", width / 2-18, height / 2);
-    text(score, width / 1.6-18, height / 2);
-    
 
-    if (score === 16) {
+    if (score === brickRows * brickColumns) {
         ball_dx = 0;
         ball_dy = 0;
-        text("Game Won", width / 2 - 18, height / 2 + 18);
+        textSize(18);
+        textAlign(CENTER);
+        text("Game Won", width / 2, height / 2);
     }
+
     if (lost) {
-        text("Game Lost", width / 2 - 18, height / 2 + 18);
+        textSize(18);
+        textAlign(CENTER);
+        text("Game Lost", width / 2, height / 2);
     }
 
-    ball_x = ball_x + ball_dx;
-    ball_y = ball_y + ball_dy;
+    ball_x += ball_dx;
+    ball_y += ball_dy;
+}
 
+function horizontalHit() {
+    return ball_x + ball_radius >= width || ball_x <= ball_radius;
+}
+
+function verticalHit() {
+    return ball_y <= ball_radius || (ball_y >= paddle_y && ball_x >= paddle_x && ball_x <= paddle_x + paddle_width);
+}
+
+
+
+function isBrickHit() {
+    let brickHit = false;
+
+    for (let c = 0; c < brickColumns; c++) {
+        for (let r = 0; r < brickRows; r++) {
+            if (!bricks[c][r].hidden) {
+                const brickTop = bricks[c][r].y;
+                const brickBottom = bricks[c][r].y + brick_height;
+                const brickLeft = bricks[c][r].x;
+                const brickRight = bricks[c][r].x + brick_width;
+
+                if (ball_y + ball_radius >= brickTop && ball_y - ball_radius <= brickBottom && ball_x + ball_radius >= brickLeft && ball_x - ball_radius <= brickRight ) {
+                    ball_dy = -ball_dy; 
+                    bricks[c][r].hidden = true;
+                    brickHit = true; 
+                }
+            }
+        }
+    }
+
+    return brickHit;
+}
+
+
+
+function gameOver() {
+    return ball_y >= height - ball_radius / 2;
+}
+
+function mousePressed() {
+    if (lost || score === brickRows * brickColumns) {
+        resetGame();
+    }
 }
